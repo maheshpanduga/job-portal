5a0824394c566487a1a71108b8c2dca5b2a7cf23
Mahesh Panduga <mpanduga@worklogix.com>
2021-01-02 22:08:51 +0530

    redux config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import JobPortal from './components/JobPortal';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+const store = createStore(null);
 function App() {
   return (
+    <Provider store = {store}>
       <JobPortal/>
+    </Provider>
+      
   );
 }
 
